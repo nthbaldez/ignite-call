c1efdb7b483cd4714879d5cddda8ffd51c90a01c
Nathan Baldez <nathanbsb2@hotmail.com>
2024-03-10 22:19:06 -0300

    feature: estrutura home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,28 @@
+import Image from 'next/image'
+import previewImage from '../assets/app-preview.png'
+import './../../global.css';
 
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <div className="max-w-[calc(100vw-(100vw-1160px)/2)] ml-auto h-screen flex items-center gap-20">
+      <div className="max-w-480 px-10">
+        <h1 className="text-6xl md:text-4xl lg:text-5xl xl:text-6xl">Agendamento Descomplicado</h1>
+        <p className="text-gray-200 text-xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </p>
+      </div>
+
+      <div className="pr-8 overflow-hidden hidden md:block">
+        <Image
+          src={previewImage}
+          height={442}
+          width={748}
+          quality={100}
+          priority
+          alt="Calendário simbolizando aplicação em funcionamento"
+        />
+      </div>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,67 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      'gray100': '#E1E1E6',
+      'gray200': '#A9A9B2',
+      'gray400': '#7C7C8A',
+      'gray500': '#505059',
+      'gray600': '#323238',
+      'gray700': '#29292E',
+      'gray800': '#202024',
+      'gray900': '#121214',
+      'ignite300': '#00B37E',
+      'ignite500': '#00875F',
+      'ignite700': '#015F43',
+      'ignite900': '#00291D'
     },
+    borderRadius: {
+      px: '1px',
+      xs: '4px',
+      sm: '6px',
+      md: '8px',
+      lg: '16px',
+      full: '99999px'  
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      fontSizes: {
+        'xxs': '0.625rem',
+        'xs': '0.75rem',
+        'sm': '0.875rem',
+        'md': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '4xl': '2rem',
+        '5xl': '2.25rem',
+        '6xl': '3rem',
+        '7xl': '4rem',
+        '8xl': '4.5rem',
+        '9xl': '6rem'
+      },
+      fontWeight: {
+        regular: '400',
+        medium: '500',
+        bold: '700'
+      },
+      fontFamily: {
+        default: 'Roboto, sans-serif',
+        code: 'monospace'
+      },
+      lineHeight: {
+        shorter: '125%',
+        short: '140%',
+        base: '160%',
+        tall: '180%'
+      },
+    }
   },
   plugins: [],
 };
